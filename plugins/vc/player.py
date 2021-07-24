89c57f2af7fe51903e1e39cef6024cc9aa0dffbe
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-24 21:02:28 +0700

    .

--- plugins/vc/player.py
+++ plugins/vc/player.py
@@ -1,29 +1,9 @@
-"""
-tgvc-userbot, Telegram Voice Chat Userbot
-Copyright (C) 2021  Dash Eclipse
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-Play and Control Audio playing in Telegram Voice Chat
-
+"""Play and Control Audio playing in Telegram Voice Chat
 Dependencies:
 - ffmpeg
-
 Required group admin permissions:
 - Delete messages
 - Manage voice chats (optional)
-
 How to use:
 - Start the userbot
 - send !join to a voice chat enabled group chat
@@ -33,75 +13,70 @@ How to use:
   can use the !play command now
 - check !help for more commands
 """
-import asyncio
 import os
+import asyncio
 from datetime import datetime, timedelta
-
-# noinspection PyPackageRequirements
-import ffmpeg
 from pyrogram import Client, filters, emoji
-from pyrogram.methods.messages.download_media import DEFAULT_DOWNLOAD_DIR
 from pyrogram.types import Message
-from pyrogram.utils import MAX_CHANNEL_ID
-from pytgcalls import GroupCallFactory, GroupCallFileAction
+from pyrogram.methods.messages.download_media import DEFAULT_DOWNLOAD_DIR
+from pytgcalls import GroupCall
+import ffmpeg
 
 DELETE_DELAY = 8
-DURATION_AUTOPLAY_MIN = 10
-DURATION_PLAY_HOUR = 3
 
 USERBOT_HELP = f"""{emoji.LABEL}  **Common Commands**:
 __available to group members of current voice chat__
 __starts with / (slash) or ! (exclamation mark)__
-
-\u2022 **/play**  reply with an audio to play/queue it, or show playlist
-\u2022 **/current**  show current playing time of current track
-\u2022 **/repo**  show git repository of the userbot
-\u2022 `!help`  show help for commands
-
-
+/play  reply with an audio to play/queue it, or show playlist
+/current  show current playing time of current track
+/repo  show git repository of the userbot
+`!help`  show help for commands
 {emoji.LABEL}  **Admin Commands**:
 __available to userbot account itself and its contacts__
 __starts with ! (exclamation mark)__
-
-\u2022 `!skip` [n] ...  skip current or n where n >= 2
-\u2022 `!join`  join voice chat of current group
-\u2022 `!leave`  leave current voice chat
-\u2022 `!vc`  check which VC is joined
-\u2022 `!stop`  stop playing
-\u2022 `!replay`  play from the beginning
-\u2022 `!clean`  remove unused RAW PCM files
-\u2022 `!pause` pause playing
-\u2022 `!resume` resume playing
-\u2022 `!mute`  mute the VC userbot
-\u2022 `!unmute`  unmute the VC userbot
+`!skip` [n] ...  skip current or n where n >= 2
+`!join`  join voice chat of current group
+`!leave`  leave current voice chat
+`!vc`  check which VC is joined
+`!stop`  stop playing
+`!replay`  play from the beginning
+`!clean`  remove unused RAW PCM files
+`!pause` pause playing
+`!resume` resume playing
+`!mute`  mute the VC userbot
+`!unmute`  unmute the VC userbot
 """
 
 USERBOT_REPO = f"""{emoji.ROBOT} **Telegram Voice Chat UserBot**
-
 - Repository: [GitHub](https://github.com/callsmusic/tgvc-userbot)
 - License: AGPL-3.0-or-later"""
 
+
 # - Pyrogram filters
 
-main_filter = (filters.group
-               & filters.text
-               & ~filters.edited
-               & ~filters.via_bot)
+main_filter = (
+    filters.group
+    & filters.text
+    & ~filters.edited
+    & ~filters.via_bot
+)
 self_or_contact_filter = filters.create(
-    lambda _, __, message:
+    lambda
+    _,
+    __,
+    message:
     (message.from_user and message.from_user.is_contact) or message.outgoing
 )
 
 
 async def current_vc_filter(_, __, m: Message):
     group_call = mp.group_call
-    if not (group_call and group_call.is_connected):
+    if not group_call.is_connected:
         return False
     chat_id = int("-100" + str(group_call.full_chat.id))
     if m.chat.id == chat_id:
         return True
     return False
-
 
 current_vc = filters.create(current_vc_filter)
 
@@ -111,8 +86,7 @@ current_vc = filters.create(current_vc_filter)
 
 class MusicPlayer(object):
     def __init__(self):
-        self.group_call = None
-        self.client = None
+        self.group_call = GroupCall(None, path_to_log_file='')
         self.chat_id = None
         self.start_time = None
         self.playlist = []
@@ -148,63 +122,42 @@ mp = MusicPlayer()
 # - pytgcalls handlers
 
 
-async def network_status_changed_handler(context, is_connected: bool):
+@mp.group_call.on_network_status_changed
+async def network_status_changed_handler(gc: GroupCall, is_connected: bool):
     if is_connected:
-        mp.chat_id = MAX_CHANNEL_ID - context.full_chat.id
+        mp.chat_id = int("-100" + str(gc.full_chat.id))
         await send_text(f"{emoji.CHECK_MARK_BUTTON} joined the voice chat")
     else:
         await send_text(f"{emoji.CROSS_MARK_BUTTON} left the voice chat")
         mp.chat_id = None
 
 
-async def playout_ended_handler(_, __):
+@mp.group_call.on_playout_ended
+async def playout_ended_handler(group_call, filename):
     await skip_current_playing()
 
 
 # - Pyrogram handlers
 
 
-@Client.on_message(
-    filters.group
-    & ~filters.edited
-    & current_vc
-    & (filters.regex("^(\\/|!)play$") | filters.audio)
-)
+@Client.on_message(main_filter & current_vc & filters.regex("^(\\/|!)play$"))
 async def play_track(client, m: Message):
     group_call = mp.group_call
     playlist = mp.playlist
-    # check audio
-    if m.audio:
-        if m.audio.duration > (DURATION_AUTOPLAY_MIN * 60):
-            reply = await m.reply_text(
-                f"{emoji.ROBOT} audio which duration longer than "
-                f"{str(DURATION_AUTOPLAY_MIN)} min won't be automatically "
-                "added to playlist"
-            )
-            await _delay_delete_messages((reply,), DELETE_DELAY)
-            return
-        m_audio = m
-    elif m.reply_to_message and m.reply_to_message.audio:
-        m_audio = m.reply_to_message
-        if m_audio.audio.duration > (DURATION_PLAY_HOUR * 60 * 60):
-            reply = await m.reply_text(
-                f"{emoji.ROBOT} audio which duration longer than "
-                f"{str(DURATION_PLAY_HOUR)} hours won't be added to playlist"
-            )
-            await _delay_delete_messages((reply,), DELETE_DELAY)
-            return
-    else:
+    # show playlist
+    if not m.reply_to_message or not m.reply_to_message.audio:
         await mp.send_playlist()
         await m.delete()
         return
     # check already added
+    m_reply = m.reply_to_message
     if playlist and playlist[-1].audio.file_unique_id \
-            == m_audio.audio.file_unique_id:
+            == m_reply.audio.file_unique_id:
         reply = await m.reply_text(f"{emoji.ROBOT} already added")
         await _delay_delete_messages((reply, m), DELETE_DELAY)
         return
     # add to playlist
-    playlist.append(m_audio)
+    playlist.append(m_reply)
     if len(playlist) == 1:
         m_status = await m.reply_text(
             f"{emoji.INBOX_TRAY} downloading and transcoding..."
@@ -221,14 +174,13 @@ async def play_track(client, m: Message):
     await mp.send_playlist()
     for track in playlist[:2]:
         await download_audio(track)
-    if not m.audio:
-        await m.delete()
+    await m.delete()
 
 
 @Client.on_message(main_filter
                    & current_vc
                    & filters.regex("^(\\/|!)current$"))
-async def show_current_playing_time(_, m: Message):
+async def show_current_playing_time(client, m: Message):
     start_time = mp.start_time
     playlist = mp.playlist
     if not start_time:
@@ -249,7 +201,7 @@ async def show_current_playing_time(_, m: Message):
 @Client.on_message(main_filter
                    & (self_or_contact_filter | current_vc)
                    & filters.regex("^(\\/|!)help$"))
-async def show_help(_, m: Message):
+async def show_help(client, m: Message):
     if mp.msg.get('help') is not None:
         await mp.msg['help'].delete()
     mp.msg['help'] = await m.reply_text(USERBOT_HELP, quote=False)
@@ -260,7 +212,7 @@ async def show_help(_, m: Message):
                    & self_or_contact_filter
                    & current_vc
                    & filters.command("skip", prefixes="!"))
-async def skip_track(_, m: Message):
+async def skip_track(client, m: Message):
     playlist = mp.playlist
     if len(m.command) == 1:
         await skip_current_playing()
@@ -277,10 +229,7 @@ async def skip_track(_, m: Message):
                     text.append(f"{emoji.WASTEBASKET} {i}. **{audio}**")
                 else:
                     text.append(f"{emoji.CROSS_MARK} {i}")
-            reply = await m.reply_text(
-                "\n".join(text),
-                disable_web_page_preview=True
-            )
+            reply = await m.reply_text("\n".join(text))
             await mp.send_playlist()
         except (ValueError, TypeError):
             reply = await m.reply_text(f"{emoji.NO_ENTRY} invalid input",
@@ -293,23 +242,19 @@ async def skip_track(_, m: Message):
                    & filters.regex("^!join$"))
 async def join_group_call(client, m: Message):
     group_call = mp.group_call
-    if not group_call:
-        mp.group_call = GroupCallFactory(client).get_file_group_call()
-        mp.group_call.add_handler(network_status_changed_handler,
-                                  GroupCallFileAction.NETWORK_STATUS_CHANGED)
-        mp.group_call.add_handler(playout_ended_handler,
-                                  GroupCallFileAction.PLAYOUT_ENDED)
-        await mp.group_call.start(m.chat.id)
-        await m.delete()
-    if group_call and group_call.is_connected:
+    group_call.client = client
+    if group_call.is_connected:
         await m.reply_text(f"{emoji.ROBOT} already joined a voice chat")
+        return
+    await group_call.start(m.chat.id)
+    await m.delete()
 
 
 @Client.on_message(main_filter
                    & self_or_contact_filter
                    & current_vc
                    & filters.regex("^!leave$"))
-async def leave_voice_chat(_, m: Message):
+async def leave_voice_chat(client, m: Message):
     group_call = mp.group_call
     mp.playlist.clear()
     group_call.input_filename = ''
@@ -322,7 +267,7 @@ async def leave_voice_chat(_, m: Message):
                    & filters.regex("^!vc$"))
 async def list_voice_chat(client, m: Message):
     group_call = mp.group_call
-    if group_call and group_call.is_connected:
+    if group_call.is_connected:
         chat_id = int("-100" + str(group_call.full_chat.id))
         chat = await client.get_chat(chat_id)
         reply = await m.reply_text(
@@ -389,7 +334,7 @@ async def resume_playing(_, m: Message):
     if mp.msg.get('pause') is not None:
         await mp.msg['pause'].delete()
     await m.delete()
-    await _delay_delete_messages((reply,), DELETE_DELAY)
+    await _delay_delete_messages((reply, ), DELETE_DELAY)
 
 
 @Client.on_message(main_filter
@@ -398,7 +343,7 @@ async def resume_playing(_, m: Message):
                    & filters.regex("^!clean$"))
 async def clean_raw_pcm(client, m: Message):
     download_dir = os.path.join(client.workdir, DEFAULT_DOWNLOAD_DIR)
-    all_fn: list[str] = os.listdir(download_dir)
+    all_fn = os.listdir(download_dir)
     for track in mp.playlist[:2]:
         track_fn = f"{track.audio.file_unique_id}.raw"
         if track_fn in all_fn:
